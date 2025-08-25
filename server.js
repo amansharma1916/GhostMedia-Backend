87288197e29580827dd337d07402a0000d77db4b
aman sharma <amansharmayt1908@gmail.com>
2025-08-25 22:27:46 +0530

    Add ping endpoint for server health check

--- server.js
+++ server.js
@@ -148,5 +148,9 @@ app.get("/", (req, res) => {
   res.send("GhostMedia server is running...");
 });
 
+app.get("/ping", (req, res) => {
+  res.send("Pong");
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT} 🚀`));
